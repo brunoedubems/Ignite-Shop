761be27992ed959cc83a27dc626fbc8d1616791b
Bruno <bemds1@aluno.ifnmg.edu.br>
2023-01-23 18:45:02 -0300

    incial

---
+++ src/pages/styles/index.ts
@@ -0,0 +1,28 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: "#fff",
+
+      "gray-100": "#E1E1E6",
+      "gray-300": "#C4C4CC",
+
+      "gray-800": "#202024",
+      "gray-900": "#121214",
+
+      "green-300": "#00B37E",
+      "green-500": "#00875F",
+
+    },
+  },
+});
